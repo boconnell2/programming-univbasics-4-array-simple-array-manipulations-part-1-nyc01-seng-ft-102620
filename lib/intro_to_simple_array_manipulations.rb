f7db81b562d6dbbd8a9534f1beb26406fa3bdac3
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-15 00:04:26 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def pop_with_args(array)
+  array.pop()
+  array.pop()
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  
+  
